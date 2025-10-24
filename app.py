03e592e70c3ae85db97808c77dfea5f11587e05e
Onur Çopur <onurcopur@PR05L035.prime-vision.local>
2025-10-24 11:33:22 +0200

    add token access

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],  # Allow frontend to read response headers
 )
 
 # In-memory storage for job execution status
